2417a4ed26f1a9e26e62a13c7b661d113264390e
Caleb Tuttle <1calebtuttle@gmail.com>
2025-08-20 15:49:58 -0400

    update response shape of facetec encryption key endpoint

--- src/services/facetec/encryption-key.js
+++ src/services/facetec/encryption-key.js
@@ -27,20 +27,11 @@ export async function getProductionEncryptionKeyText(req, res) {
       });
     }
 
-    // Convert YAML text to JSON. The response looks something like this
-    // "  appToken    = dscV7M...\n  expiryDate  = 2025-09-10\n  key         = 0030...\n",
     const encryptionKeyStr = resp.data.encryptionKeyText
-    // Create an array that looks like ['appToken', 'dsc...', ...]
-    const arr = encryptionKeyStr.trim().split(/=|\n/).map(x=> x.trim())
-    // Create a json object that looks like this { appToken: 'dsc...', ... }
-    const json = arr.reduce((acc, curr, idx) => {
-      if (idx % 2 === 0) {
-        acc[curr] = arr[idx + 1];
-      }
-      return acc;
-    }, {})
 
-    return res.status(200).json(json)
+    return res.status(200).json({
+      encryptionKeyText: encryptionKeyStr
+    })
   } catch (err) {
     if (err.request) {
       console.error(
